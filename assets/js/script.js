71c8bd27f222f98389da3195419bac1a666c4b14
mrityunjay <mrityunjay.83039@gmail.com>
2024-01-14 11:44:49 +0530

    Prepared event listner on comment container for event handeling

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+let comment_container = document.getElementById('comment-container');
+
+comment_container.addEventListener('click',function(e){
+    let replyBtnClick = e.target.classList.contains('add-reoly');
+    let submitBtnClic = e.target.classList.contains('submit-btn');
+
+    if(replyBtnClick){
+
+    }
+
+    if(submitBtnClic){
+
+    }
+})
